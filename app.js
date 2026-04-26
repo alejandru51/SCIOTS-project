314091c73e75643809ce2cb66bae5204267efd53
alejandru51 <alejandroacedoa@gmail.com>
2026-04-26 12:31:41 +0200

    Setup inicial: Express y primer post

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ mensaje: 'Super Server Funcionando0000000' })
+})
+
+app.post('/api/data', (req, res) => {
+  console.log('JSON recibido:', req.body)
+  res.json({
+    ok: true,
+    recibido: req.body
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`)
+})
